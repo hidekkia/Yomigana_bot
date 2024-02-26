e80803ecbfc953c13467dc4ea75e7a232753b68f
Vitor Hideki <122884946+hidekkia@users.noreply.github.com>
2024-02-26 09:00:32 -0300

    Delete Yomigana_bot directory

--- Yomigana_bot/asdf.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-import time
-
-driver = webdriver.Chrome() # Cria interação entre o código e o site do Chrome
-driver.get("https://www.clubdam.com/karaokesearch/") #Carregar site em que os dados serão coletados
-time.sleep(10)
